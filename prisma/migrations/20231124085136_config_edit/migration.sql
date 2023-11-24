e27fdc5250206e2752c0d2b78d39d597251e1841
daniel <danielemmanuel257@gmail.com>
2023-11-24 11:11:43 +0100

    added other fields

---
+++ prisma/migrations/20231124085136_config_edit/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "config" ADD COLUMN     "currency" TEXT,
+ADD COLUMN     "network" TEXT,
+ADD COLUMN     "wallet_address" TEXT;
--- src/config/DTO/Config.dto.ts
+++ src/config/DTO/Config.dto.ts
@@ -1,8 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class ConfigDTO {
   @ApiProperty()
   @IsNumber()
   fee: number;
+
+  @ApiProperty()
+  @IsString()
+  wallet_address: string;
+
+  @ApiProperty()
+  @IsString()
+  network: string;
+
+  @ApiProperty()
+  @IsString()
+  currency: string;
 }
